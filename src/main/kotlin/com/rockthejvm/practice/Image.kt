e2feccc1fa25f76becaba01cdacdc77c0621df96
AdamMudrak <adam.mudrak@exirom.com>
2025-10-06 16:21:09 +0300

    Pre-done commit

--- src/main/kotlin/com/rockthejvm/practice/Image.kt
+++ src/main/kotlin/com/rockthejvm/practice/Image.kt
@@ -1,5 +1,6 @@
 package com.rockthejvm.practice
 
+import com.rockthejvm.practice.Image.Companion.loadResource
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
@@ -12,7 +13,32 @@ class Image (val buffImage: BufferedImage) {
         ImageIO.write(buffImage, "JPG", File(path))
 
     fun saveResources(path: String) =
-        save("src/main/resources/$path")
+        save("src/main/resources/$path.jpg")
+
+    /*
+    * 1. Check dimensions - return null if any dimensaion is invalid
+    * 2. Create a black image of width * height
+    * 3. Iterate through coords x ..< x + w, y ..< y + h
+    *       - use buffImage.getRGB to get a pixel from original image;
+    *       - use resultImage.buffImage.setRGB to set a pixel in the result;
+    *       - calculate the coordinates;
+    * 4. Return the result image
+    * */
+    fun crop(x: Int, y: Int, width: Int, height: Int): Image? {
+        val black = black(width, height)
+        var blackX = 0
+        for (countX in x ..< x + width) {
+            var blackY = 0
+            for (countY in y ..< y + height) {
+                val currentPixel = buffImage.getRGB(countX, countY)
+                black.buffImage.setRGB(blackX, blackY, currentPixel)
+                blackY++
+            }
+            blackX++
+        }
+
+        return black
+    }
 
     companion object {
         fun black(width: Int, height: Int): Image {
@@ -32,6 +58,8 @@ class Image (val buffImage: BufferedImage) {
 object ImagePlayground {
     @JvmStatic
     fun main(args: Array<String>) {
-        Image.black(100, 100).saveResources("black.jpg")
+        val loadedImage = Image(loadResource("testimage.jpg"))
+        val crop = loadedImage.crop(125, 125, 256, 256)
+        crop?.saveResources("cropped")
     }
 }
