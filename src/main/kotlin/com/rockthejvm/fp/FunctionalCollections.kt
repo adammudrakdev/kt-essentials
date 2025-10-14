086820f3833e64d05bce15e0db40b8704116cef6
AdamMudrak <adam.mudrak@exirom.com>
2025-10-14 03:04:43 +0300

    Functional Collections done

--- src/main/kotlin/com/rockthejvm/fp/FunctionalCollections.kt
+++ src/main/kotlin/com/rockthejvm/fp/FunctionalCollections.kt
@@ -49,9 +49,69 @@ object FunctionalCollections {
         println(stringRep)
     }
 
+    fun demoSets() {
+        val numbers = setOf(1, 2, 3, 4, 5)
+
+        //check whether all set is true to predicate
+        val lessThanTen = numbers.all { it < 10 }
+        val noneBiggerThanTen = numbers.none { it > 10 }
+
+        println(lessThanTen)
+        println(noneBiggerThanTen)
+    }
+
+    fun demoMaps() {
+        val phonebook = mapOf("Adam" to 123, "Iuliia" to 456, "Or" to 789)
+
+        //filter keys
+        val letterA = phonebook.filterKeys { it.startsWith("A") }
+
+        //map values
+        val addSuffix = phonebook
+            .mapValues { pair -> pair.value * 10}
+            .mapValues { pair -> pair.key + pair.value }
+
+        println(addSuffix)
+
+        val phonebookWithDefault = phonebook.withDefault { key ->
+            println("No entry for $key, returning default")
+            -9000
+        }
+
+        println(phonebookWithDefault.getValue("RANDOM"))
+    }
+
 
     @JvmStatic
     fun main(args: Array<String>) {
-        demoLists()
+        //practice
+        //ex 1
+        val testList: List<String> = listOf("I", "want", "to", "test", "lambda")
+        val getListOfLengths = { l: List<String> -> l.map { s -> s.length } }
+        println(getListOfLengths(testList))
+
+        //ex 2
+        val numbers1 = listOf(1, 2, 3, 4, 5)
+        val numbers2 = listOf(2, 4, 6, 8, 10)
+        val getSumOfTwoLists = { list1: List<Int>, list2: List<Int> -> list1.zip(list2).map { it.first + it.second } }
+        val getSumOfTwoListsV2 = { list1: List<Int>, list2: List<Int> -> list1.zip(list2) { a, b -> a + b } }
+        println(getSumOfTwoLists(numbers1, numbers2))
+        println(getSumOfTwoListsV2(numbers1, numbers2))
+
+        //ex 3
+        val mixLists = { list1: List<Any>, list2: List<Any> -> list1.flatMap {
+            el1 -> list2.map { "$el1-$it" }
+        }}
+        println(mixLists(testList, numbers1))
+
+        //ex 4
+        val listToStringFold = { list: List<String> -> list.fold(""){ a, b -> a + b } }
+        val listToStringReduce = { list: List<String> -> list.reduce { a, b -> a + b } }
+        println(listToStringReduce(testList))
+        println(listToStringFold(testList))
+
+        //ex 5
+        val stringRepeater = { string: String, n: Int -> string.repeat(n)}
+        println(stringRepeater("Kotlin", 5))
     }
 }
